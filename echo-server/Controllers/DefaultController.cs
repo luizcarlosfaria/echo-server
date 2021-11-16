d57660b6cb22dc3a77f526ba6b0a8f7367bd025a
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2021-11-16 08:07:55 -0300

    Adição do JSON e revisão da view

--- echo-server/Controllers/DefaultController.cs
+++ echo-server/Controllers/DefaultController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json.Linq;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace Jornada.Controllers
 {
@@ -18,12 +23,12 @@ namespace Jornada.Controllers
         private bool Accept(string format) => this.Request.Headers["Accept"].FirstOrDefault()?.Contains(format) ?? false;
 
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             if (this.Accept("text/html"))
                 return this.IndexHtml();
             else if (this.Accept("application/json") || this.Accept("*/*"))
-                return this.IndexJson();
+                return await this.IndexJsonAsync();
             else
                 return this.View();
         }
@@ -34,23 +39,39 @@ namespace Jornada.Controllers
         }
 
         string[] verbsWithForms = new string[] { "POST", "PUT" };
-        string[] contentTypeWithoutForms = new string[] { "application/json" };
+        string[] contentTypeWithoutForms = new string[] { "application/json", null };
+        string[] contentJson = new string[] { "application/json" };
 
-        public IActionResult IndexJson()
+        public async Task<IActionResult> IndexJsonAsync()
         {
-            var responseObject = new
-            {
-                Headers = this.Request.Headers,
-                Path = this.Request.Path,
-                Method = this.Request.Method,
-                Scheme = this.Request.Scheme,
-                QueryString = this.Request.Query,
-                Forms = this.verbsWithForms.Contains(this.Request.Method) && !this.contentTypeWithoutForms.Contains(this.Request.ContentType) ? this.Request.Form : null,
-                EnvironmentVariables = Environment.GetEnvironmentVariables()
-            };
-            return this.Json(responseObject);
+            JObject returnValue = new JObject();
+            returnValue.Add("Headers", JObject.FromObject(this.Request.Headers));
+            returnValue.Add("Path", new JValue(this.Request.Path));
+            returnValue.Add("MachineName", new JValue(Environment.MachineName));
+            returnValue.Add("Method", new JValue(this.Request.Method));
+            returnValue.Add("Scheme", new JValue(this.Request.Scheme));
+            returnValue.Add("QueryString", JArray.FromObject(this.Request.Query));
+            returnValue.Add("Forms", JArray.FromObject(this.verbsWithForms.Contains(this.Request.Method)
+                                && this.contentTypeWithoutForms.Contains(this.Request.ContentType)
+                                    ? new FormCollection(null, null) : this.Request.Form));
+            await this.SetPayload(returnValue);
+            returnValue.Add("EnvironmentVariables", JObject.FromObject(Environment.GetEnvironmentVariables()));
+
+            string returnJson = returnValue.ToString();
+
+            return this.Content(returnJson, this.contentJson.First());
         }
 
-
+        private async Task SetPayload(JObject returnValue)
+        {
+            if (this.contentJson.Contains(this.Request.ContentType))
+            {
+                this.Request.EnableBuffering();
+                var buffer = new byte[Convert.ToInt32(this.Request.ContentLength)];
+                await this.Request.Body.ReadAsync(buffer, 0, buffer.Length);
+                string json = Encoding.UTF8.GetString(buffer);
+                returnValue.Add("Payload", JObject.Parse(json));
+            }
+        }
     }
 }
